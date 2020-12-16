1afcb6884c39ed58cb37cbb454477df363deb942
jason.lu <jason-lu.mbp>
2020-12-16 18:14:11 +0800

    Add new files.

---
+++ new.py
@@ -0,0 +1,36 @@
+from efficient_apriori import apriori
+import csv
+
+if __name__ == "__main__":
+    # transactions = [('eggs', 'bacon', 'soup'),
+    #                 ('eggs', 'bacon', 'apple'),
+    #                 ('soup', 'bacon', 'banana')]
+
+    transactions = []
+    with open('utf8.csv', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        curid = "0"
+        cur_list = []
+        for row in spamreader:
+            # print(', '.join(row))
+            # print(row)
+            if (row[0] == curid):
+                cur_list.append(row[1])
+                continue
+            else:
+                if len(cur_list) >= 2:
+                    cur_tuple = tuple(cur_list)
+                    transactions.append(cur_tuple)
+                cur_list = [row[1]]
+                curid = row[0]
+
+transactions = transactions[1:4]
+
+itemsets, rules = apriori(transactions, min_support=0.2, min_confidence=1)
+
+# Print out every rule with 2 items on the left hand side,
+# 1 item on the right hand side, sorted by lift
+rules_rhs = filter(lambda rule: len(rule.lhs) == 2 and len(rule.rhs) == 1, rules)
+for rule in sorted(rules_rhs, key=lambda rule: rule.lift):
+  print(rule)  # Prints the rule and its confidence, support, lift, ...
+
